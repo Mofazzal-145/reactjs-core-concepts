769c69c8689391432a1695da53b3f13aea0b918f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-16 05:47:17 +0600

    add third example

--- src/App.js
+++ src/App.js
@@ -70,6 +70,38 @@ function Friend (props) {
     </div>
   )
 }
+// ex: 03
+/* function App() {
+  return (
+    <div className="App">
+    <Person name = "Shakib" secondPerson = "Opu"></Person>
+    <Person name = "Omar" secondPerson = "mowsumi" ></Person>
+    <Person name = "Razzak" secondPerson = "Sabana"></Person>
+  
+    <h4>New Components: </h4>
+    <Friend name = "kabila" phone = "0124564"></Friend>
+    <Friend name = "sabila" phone = "541331"></Friend>
+    <Friend name = "mabila" phone = "0001122"></Friend>
+    </div>
+  );
+}
+function Person(props) {
+  console.log(props)
+  return (
+    <div className='person'>
+    <h1>Name:{props.name}</h1>
+    <p>{props.secondPerson}</p>
+    </div>
+  )
+}
 
+function Friend (props) {
+  return (
+    <div className="friend">
+      <h2>Name:{props.name}</h2>
+      <p>Phone No: {props.phone}</p>
+    </div>
+  )
+} */
 
 export default App;
