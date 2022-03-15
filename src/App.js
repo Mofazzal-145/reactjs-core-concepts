051e7272eb9f703c60f37aa80ae78977bc6721b3
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-16 02:36:18 +0600

    first file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 173;
+const singer = {name: 'Dr. Mahfuz', job: 'Singer'};
+const femaleSinger = {name: 'Eva Rahman', job: 'Female Singer'};
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white',
+  borderRadius: '10px'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Person name = "Shakib" secondPerson = "Opu"></Person>
+    <Person name = "Omar" secondPerson = "mowsumi" ></Person>
+    <Person name = "Razzak" secondPerson = "Sabana"></Person>
+  
+    <h4>New Components: </h4>
+    <Friend name = "kabila" phone = "0124564"></Friend>
+    <Friend name = "sabila" phone = "541331"></Friend>
+    <Friend name = "mabila" phone = "0001122"></Friend>
     </div>
   );
+}
+function Person(props) {
+  console.log(props)
+  return (
+    <div className='person'>
+    <h1>Name:{props.name}</h1>
+    <p>{props.secondPerson}</p>
+    </div>
+  )
+}
+
+function Friend (props) {
+  return (
+    <div className="friend">
+      <h2>Name:{props.name}</h2>
+      <p>Phone No: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
